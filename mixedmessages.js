e20f9d799bcbafef723ed4425c5ab9f992a220e4
endicottisaac <endicottkai@gmail.com>
2022-10-04 10:58:42 -0600

    bug fix, rand person

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,4 +1,46 @@
-let randNum = 
+const randNumOne = Math.floor(Math.random() * 11)
+const randNumTwo = Math.floor(Math.random() * 11)
+const randNumThree = Math.floor(Math.random() *11)
+
+const firstWord = randNum => {
+    switch(randNum){
+        case 1:
+          return 'Family'
+          break;
+        case 2: 
+          return 'Friend'
+          break;
+        case 3: 
+          return 'Foe'
+          break;
+        case 4: 
+          return 'Father'
+          break;
+        case 5: 
+          return 'Teacher'
+          break;
+        case 6: 
+          return 'Sibling'
+          break;
+        case 7: 
+          return 'Boss'
+          break;
+        case 8: 
+          return 'Child'
+          break;
+        case 9: 
+          return 'Mother'
+          break;
+        default:
+          return 'Inner self' 
+
+        }
+    };
+
+const person = firstWord(randNumOne);
+console.log(person)
+
+
 let element = document.querySelector('button');
 
 function receivedQuote(){
@@ -9,4 +51,3 @@ function receivedQuote(){
 } 
 
 element.onclick = receivedQuote;
-
